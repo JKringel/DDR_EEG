bfe613bcb95af591a2e9b8307ff2d34219615713
jrk12003 <jackson.kringel@uconn.edu>
2016-04-19 00:43:09 -0400

    added class for game logic

--- ddr.py
+++ ddr.py
@@ -7,18 +7,26 @@ from Queue import Queue
 def main():
 
 	arrowGenQueue = Queue()
+	inputQueue = Queue()
 	win = DDRWindow("DDR", 500, 500)
-	thread = Thread(target = generateArrows, args=(2, arrowGenQueue,))
-	thread.start()
+	Genthread = Thread(target = generateArrows, args=(2, arrowGenQueue,))
+	inputThread = Thread(target = readInput, args=(inputQueue,))
+	Genthread.start()
+	inputThread.start()
 	win.startGame(2, arrowGenQueue)
 
 
 def generateArrows(waitTime, q):
-
 	while True:
 		d = random.randint(0,3)
 		q.put(d)
-		time.sleep(waitTime)		
+		time.sleep(waitTime)
+
+def readInput(q):
+	while True:
+		key = input(": ")
+		q.put(key - 1)
+
 
 if __name__ =="__main__": main()
 
---
+++ gameLogicHandler.py
@@ -0,0 +1,4 @@
+
+class GameLogicHandler():
+	def startLogic(self, arrowQueue, inputQueue):
+		
