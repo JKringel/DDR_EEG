9b53e87190edb5f8c99fc76251e026b3f0e393c4
Connor Ginley <crazyskier23@Connors-MacBook-Pro.local>
2016-04-21 18:00:30 -0400

    Fix merge

--- ddr.py
+++ ddr.py
@@ -10,27 +10,7 @@ def main():
 	gameLogic = GameLogicHandler(arrowQueue)
 	logicHandlerThread = Thread(target = gameLogic.threadInit)
 	win = DDRWindow("DDR", 500, 500)
-<<<<<<< HEAD
-	Genthread = Thread(target = generateArrows, args=(2, arrowGenQueue,))
-	inputThread = Thread(target = readInput, args=(inputQueue,))
-	Genthread.start()
-	inputThread.start()
 	win.startGame(2, arrowGenQueue)
-
-
-def generateArrows(waitTime, q):
-	while True:
-		d = random.randint(0,3)
-		q.put(d)
-		time.sleep(waitTime)
-
-def readInput(q):
-	while True:
-		# key = input(": ")
-		q.put(key - 1)
-=======
-	win.startGame(2, arrowQueue)
->>>>>>> 756d15c012a64ae5d4c9c6132a60173f0c03c3e7
 
 
 if __name__ =="__main__": main()
