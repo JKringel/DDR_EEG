756d15c012a64ae5d4c9c6132a60173f0c03c3e7
jrk12003 <jackson.kringel@uconn.edu>
2016-04-21 17:55:00 -0400

    improving gameLogicHandler

--- ddr.py
+++ ddr.py
@@ -3,29 +3,14 @@ import random
 import time
 from gameView import DDRWindow
 from Queue import Queue
+from gameLogicHandler import GameLogicHandler
 
 def main():
-
-	arrowGenQueue = Queue()
-	inputQueue = Queue()
+	arrowQueue = Queue()
+	gameLogic = GameLogicHandler(arrowQueue)
+	logicHandlerThread = Thread(target = gameLogic.threadInit)
 	win = DDRWindow("DDR", 500, 500)
-	Genthread = Thread(target = generateArrows, args=(2, arrowGenQueue,))
-	inputThread = Thread(target = readInput, args=(inputQueue,))
-	Genthread.start()
-	inputThread.start()
-	win.startGame(2, arrowGenQueue)
-
-
-def generateArrows(waitTime, q):
-	while True:
-		d = random.randint(0,3)
-		q.put(d)
-		time.sleep(waitTime)
-
-def readInput(q):
-	while True:
-		key = input(": ")
-		q.put(key - 1)
+	win.startGame(2, arrowQueue)
 
 
 if __name__ =="__main__": main()
--- gameLogicHandler.py
+++ gameLogicHandler.py
@@ -1,4 +1,33 @@
-
+from threading import Thread
+from Queue import Queue
+import random
+import time
 class GameLogicHandler():
-	def startLogic(self, arrowQueue, inputQueue):
+
+	def __init__(self, q):
+		self.arrowToViewQueue = q
+		self.arrowFromGenQueue = Queue()
+		self.Genthread = Thread(target = self.generateArrows, args=(2, self.arrowFromGenQueue,))
+
+	def threadInit(self, q):
+		self.Genthread.start()
+		while True:
+			try:
+			   	direction = self.arrowFromGenQueue.get()
+		   	   	self.arrowToViewQueue.put(direction)
+		   	   	self.arrowFromGenQueue.task_done()
+			except Queue.Empty:
+			   	pass
+
+	def generateArrows(self, waitTime, q):
+		while True:
+			d = random.randint(0,3)
+			q.put(d)
+			time.sleep(waitTime)
+
+	def readInput(self, q):
+		while True:
+			key = input(": ")
+			q.put(key - 1)
+
 		
