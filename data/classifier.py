cdcfb7afda7a80e2868dbe02f7a9525f47f7b1e6
Connor Ginley <crazyskier23@Connors-MacBook-Pro.local>
2016-05-05 22:52:19 -0400

    Trying to fix bug

--- data/classifier.py
+++ data/classifier.py
@@ -6,11 +6,12 @@ class Classifier():
 	def __init__(self, n, w):
 		self.n_neighbors = n
 		self.weights = w
+		self.model = None
 
 	def trainData(self, dataSamples):
 		data = []
 		target = []
-		
+
 		# get the data nd the targets
 		for i in len(dataSamples):
 			sample = dataSamples[i]
@@ -18,8 +19,12 @@ class Classifier():
 			target.append(data.getDirection())
 
 		# create an instance of Neighbours Classifier and fit the data.
-    	clf = neighbors.KNeighborsClassifier(self.n_neighbors, weights=self.weights)
+		n = self.n_neighbors
+		w = self.weights
+    	clf = neighbors.KNeighborsClassifier(n, weights=w)
     	clf.fit(data, target)
+
+    	self.model = clf
 
 
 		
